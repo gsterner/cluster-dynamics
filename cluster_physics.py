f92a561a5a192e98a6312765a87aaf5eb0baacd0
Gustaf Sterner <gsterner@gmail.com>
2013-05-06 22:10:23 +0200

    Implemented functions for rate constants and jump rates

--- cluster_physics.py
+++ cluster_physics.py
@@ -4,8 +4,26 @@ AVOGADRO = 6.022141e23
 BOLTZMANN = 1.3806e-23
 TEMP_INDEP_DIFFUSIVITY = 2e9
 ACTIVATION_ENERGY = 4.4e5
+JUMP_DISTANCE = 1e-10
 
 def diffusivity(temperature):
     return TEMP_INDEP_DIFFUSIVITY * math.exp(-ACTIVATION_ENERGY/BOLTZMANN/AVOGADRO/temperature)
 
+def unbiased_jump_rate(temperature):
+    return 6 * diffusivity(temperature) /(JUMP_DISTANCE**JUMP_DISTANCE)
+
+def number_of_sites_transformation(number_of_molecules):
+    return 4 * number_of_molecules ** (2/3)
+
+def jump_rate(temperature):
+    return unbiased_jump_rate(temperature)
+
+def rate_constant(number_of_molecules, temperature):
+    return number_of_sites_transformation(number_of_molecules) * jump_rate(temperature)
+
+def forward_rate_constant(number_of_molecules, temperature):
+    return rate_constant(number_of_molecules, temperature)
+
+def backward_rate_constant(number_of_molecules, temperature):
+    return rate_constant(number_of_molecules - 1, temperature)
 
